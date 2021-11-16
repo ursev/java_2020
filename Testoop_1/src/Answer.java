85e17688a791dc6d155c8ba3ce8e550561ebd37a
ursev <92460468+ursev@users.noreply.github.com>
2021-11-16 22:38:01 +0300

    Add files via upload

---
+++ Testoop_1/src/Answer.java
@@ -0,0 +1,11 @@
+public class Answer {
+    private String nameAnswer;
+
+    public Answer(String nameAnswer) {
+        this.nameAnswer = nameAnswer;
+}
+
+    public String getNameAnswer() {
+        return nameAnswer;
+    }
+}
---
+++ Testoop_1/src/Question.java
@@ -0,0 +1,21 @@
+public class Question {
+    private String nameQuestion;
+    private Answer[] listAnswers;
+    private Answer correctAnswer;
+
+    public Question(String nameQuestion, Answer[] listAnswers, Answer correctAnswer) {
+        this.nameQuestion = nameQuestion;
+        this.listAnswers = listAnswers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getNameQuestion() {
+        return nameQuestion;
+    }
+    public Answer[] getListAnswers() {
+        return listAnswers;
+    }
+    public Answer getCorrectAnswer() {
+        return correctAnswer;
+    }
+}
