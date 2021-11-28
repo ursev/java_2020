0de48ba71b051c7d808b84cc3d2afcbe7321b22c
ursev <92460468+ursev@users.noreply.github.com>
2021-11-28 15:43:01 +0300

    Delete Bank.java

--- bank/src/Bank.java
+++
@@ -1,8 +0,0 @@
-//import java.util.List;
-
-//public interface Bank {
-//    void add(Client client, Account account);
-//    List<Account> getAccounts(Client client);
-    //Client findClient(Account account);
-//}
-
